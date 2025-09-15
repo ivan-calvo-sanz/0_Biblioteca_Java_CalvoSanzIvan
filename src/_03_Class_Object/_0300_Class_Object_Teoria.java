32aa9aaf15f13bb8d496e4fc55294075f3b7f2bb
ivan-calvo-sanz <ivan.calvo.sanz@gmail.com>
2025-09-15 20:49:42 +0200

    Repositorio_250915_2

---
+++ src/_03_Class_Object/_0300_Class_Object_Teoria.java
@@ -0,0 +1,22 @@
+package _03_Class_Object;
+
+/**
+ *
+ * @author Iván Calvo Sanz
+ *
+ */
+public class _0300_Class_Object_Teoria {
+
+    // CONCEPTOS BASICOS CLASE Object
+    // Todos los Objetos en Java Heredan de la Clase Object por lo tanto todos los metodos de Object estan disponibles en el resto de Clases
+    public static void main(String[] args) {
+
+        // METODOS CLASE Object
+        // .clone()                                         (return Object)     crea y devuelve una copia del objeto actual
+        // .equals(Object obj)                              (return boolean)    Compara el objeto actual con el objeto dado y devuelve verdaero si ambos objetos son iguales
+        // .getClass()                                                          Devuelve la Clase del Objeto actual
+        // .hasCode()                                                           Devuelve un código hash único para el objeto actual
+        // .toString()                                                          Devuelve una representación de cadena del Objeto
+    }
+
+}
