d8a8bfb930682955dd6f57d0f65e5187a171566e
ivan-calvo-sanz <ivan.calvo.sanz@gmail.com>
2025-09-15 16:53:14 +0200

    Repositorio_250915

---
+++ src/_10_POO_Herencia_02_Polimorfismo/Perro.java
@@ -0,0 +1,14 @@
+package _10_POO_Herencia_02_Polimorfismo;
+
+/**
+ *
+ * @author ivanc
+ */
+public class Perro extends Animal {
+
+    @Override
+    public void hacerRuido() {
+        System.out.println("El Perro ladra");
+    }
+
+}
---
+++ src/_10_POO_Herencia_02_Polimorfismo/Pitbull.java
@@ -0,0 +1,14 @@
+package _10_POO_Herencia_02_Polimorfismo;
+
+/**
+ *
+ * @author ivanc
+ */
+public class Pitbull extends Perro {
+
+    @Override
+    public void hacerRuido() {
+        System.out.println("El Pitbull tiene un ladrido profundo");
+    }
+
+}
